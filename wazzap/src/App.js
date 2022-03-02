77f08fda3af085f05460f7b8a9410bb58c775302
= <=>
2022-03-01 22:42:39 -0600

    start finished

--- wazzap/src/App.js
+++ wazzap/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Start from './start/Start';
 
 function App() {
+
+  const [token, setToken] = useState('');
+  const [instance, setInstance] = useState('');
+
+  const loadInfo = () => {
+    console.log("Token: ", token, "Instance: ", instance);
+  };
+
   return (
     <>
-      <Start />
+      <Start token={token} setToken={setToken} instance={instance} setInstance={setInstance} start={loadInfo} />
     </>
   );
 }
--- wazzap/src/start/Start.jsx
+++ wazzap/src/start/Start.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './start.css';
 
-export default function Start() {
+export default function Start({ instance, setInstance, token, setToken, start }) {
   return (
     <div className="start">
         <div className="input__item">
             <span>Instancia:</span>
-            <input type="text"/>
+            <input type="text" value={instance} onChange={e => setInstance(e.target.value)}/>
         </div>
         <div className="input__item">
             <span>Token:</span>
-            <input type="text"/>
+            <input type="text" value={token} onChange={e => setToken(e.target.value)}/>
         </div>
 
-        <button>Iniciar</button>
+        <button onClick={start}>Iniciar</button>
     </div>
   )
 }
